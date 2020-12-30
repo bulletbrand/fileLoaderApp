634b23e2af09f320f8b0d7f86fc153dd3a76bc8f
Alexandr <alleksandrmatyka@gmail.com>
2020-12-30 03:16:17 +0300

    Project init

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const config = require("config")
+
+const app = express()
+
+const start = () => {
+    const port =
+
+    try {
+
+    } catch(e) {
+
+    }
+}
